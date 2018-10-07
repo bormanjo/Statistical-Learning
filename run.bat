cebc708b1f7583eec0ecd0211c2dba135a418780
J-C Borman <borman.johncraig@gmail.com>
2018-10-07 13:33:10 -0400

    Added Auto.data

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 docker build -t r_stat_dev ./dev_tools/
-docker run --rm -it -d -e ROOT=TRUE -e PASSWORD=rstudio -p 8787:8787 -v "%cd%":/home/rstudio/ r_stat_dev 
+docker run -it -d -e ROOT=TRUE -e PASSWORD=rstudio -p 8787:8787 -v "%cd%":/home/rstudio/ r_stat_dev 
 start chrome --new-window "http://localhost:8787"
